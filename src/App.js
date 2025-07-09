9349b0e9e7ce4765b8a1ece2bbb2480c9053e426
ALOK <alokakc012@gmail.com>
2025-07-09 13:07:32 +0530

    some changes in app.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello Chandan Tiwari!
+          Hello Alok Chauhan!
         </a>
       </header>
     </div>
